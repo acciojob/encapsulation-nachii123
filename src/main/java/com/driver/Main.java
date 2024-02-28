393a81f00be58fa9fb0f3bacd6b8e94dd39ba537
unknown <nachiket919156@gmail.com>
2024-02-29 00:47:04 +0530

    capsul

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+        rwOnly.setName("Tushar Thombare");
+        System.out.println(rwOnly.getName());
+    }
   
 }
